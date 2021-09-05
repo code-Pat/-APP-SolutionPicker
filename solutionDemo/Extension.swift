7926ac1bfec8b443c99796a8fc047846a004a89e
Donggeun Lee <34597321+code-Pat@users.noreply.github.com>
2021-09-05 19:57:22 +0900

    Update Extension.swift

--- solutionDemo/Extension.swift
+++ solutionDemo/Extension.swift
@@ -2,9 +2,6 @@
 //  Extension.swift
 //  ShortcutsCollectionView
 //
-//  Created by Hassan El Desouky on 12/13/18.
-//  Copyright © 2018 Hassan El Desouky. All rights reserved.
-//
 
 import UIKit
 
